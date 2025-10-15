f94cdc3642a8ce1ee5c9ab8c1e4e049d23fa0bc5
kanbanfeed <kanbanfeed@alatreeventures.com>
2025-10-15 12:48:02 +0530

    Pushing Mohit branch code into new repo in kanbanfeed github account

--- src/Components/FloatingInput.jsx
+++ src/Components/FloatingInput.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 
+
+
 export default function FloatingInput({ label, name, type = "text", formData, setFormData }) {
   return (
     <div className="relative mt-8">
